b620f6d2fc2d5a4977dba0594fe4bc3aa591cb61
Muhammad Huzaifa <37955976+Ch-M-Huzaifa@users.noreply.github.com>
2023-10-20 13:17:59 +0500

    Update TaskRepository.java

--- src/main/java/com/catagorymanagement/Repository/TaskRepository.java
+++ src/main/java/com/catagorymanagement/Repository/TaskRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Date;
 
 public interface TaskRepository extends JpaRepository<Task,Integer> {
-    Task findByDate(Date scheduleDate);
+    Task findByScheduleDate(Date scheduleDate);
 
 }
