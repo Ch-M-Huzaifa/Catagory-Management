f13ee36e975d7fcaa12d8a981fdea03688980c5c
Muhammad Huzaifa <37955976+Ch-M-Huzaifa@users.noreply.github.com>
2023-10-26 19:22:06 +0500

    Update CategoryService.java

--- src/main/java/com/catagorymanagement/Service/CategoryService.java
+++ src/main/java/com/catagorymanagement/Service/CategoryService.java
@@ -14,8 +14,7 @@ import java.util.List;
 public class CategoryService {
     @Autowired
     private CategoryRepository repository;
-    @Autowired
-    private TaskRepository taskRepository;
+
 
     public Category saveCategory(Category category){
         return repository.save(category);
