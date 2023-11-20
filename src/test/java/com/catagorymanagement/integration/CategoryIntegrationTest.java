c1b7a54837c0f96f9af7453180cb38e02674c622
Huzaifa <huzaifadalera@gmail.com>
2023-11-20 15:13:26 +0500

    updated category and task integration tests E492: Not an editor command: qw

--- src/test/java/com/catagorymanagement/integration/CategoryIntegrationTest.java
+++ src/test/java/com/catagorymanagement/integration/CategoryIntegrationTest.java
@@ -2,71 +2,112 @@ package com.catagorymanagement.integration;
 
 import com.catagorymanagement.Entity.Category;
 import com.catagorymanagement.Service.CategoryService;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class CategoryIntegrationTest extends BaseIntegrationTest {
+
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private CategoryService categoryService;
-
-    @BeforeEach
-    public void setUp() {
-        Category category = new Category(1, "Furniture");
-        categoryService.saveCategory(category);
-    }
-
+    @Autowired
+    private ObjectMapper objectMapper;
     @Test
     public void SaveCategoryIntegration() throws Exception {
         String payload = "{\"name\":\"Plastic\"}";
-
-        mockMvc.perform(post("/category/registerCat")
+       MvcResult result= mockMvc.perform(post("/category/registerCat")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(payload))
                 .andExpect(status().isOk())
                 .andReturn();
+       String responseContent =result.getResponse().getContentAsString();
+        Category category=objectMapper.readValue(responseContent, Category.class);
+        Assertions.assertEquals("Plastic",category.getName());
+        categoryService.deleteCategory(category.getId());
     }
 
     @Test
     void getAllCategories() throws Exception {
+        Category category1=new Category();
+        category1.setName("Cars");
+        categoryService.saveCategory(category1);
 
-        mockMvc.perform(get("/category/allCategories"))
+        Category category2=new Category();
+        category2.setName("Bikes");
+        categoryService.saveCategory(category2);
+
+        List<Category> categories=new ArrayList<>();
+        categories.add(category1);
+        categories.add(category2);
+
+        MvcResult result=mockMvc.perform(get("/category/allCategories"))
                 .andExpect(status().isOk())
                 .andReturn();
+        String responseContent=result.getResponse().getContentAsString();
+        List<Category> categoryList=objectMapper.readValue(responseContent, new TypeReference<List<Category>>(){});
+        Assertions.assertEquals(category1.getName(),categoryList.get(0).getName());
+        Assertions.assertEquals(category2.getName(),categoryList.get(1).getName());
+
+        for (Category category:categoryList){
+            categoryService.deleteCategory(category.getId());
+        }
     }
 
     @Test
     void getCategoriesBYID() throws Exception {
+    Category category=new Category();
+    category.setName("Vehicle");
+    categoryService.saveCategory(category);
 
-        mockMvc.perform(get("/category/cat/1"))
+    MvcResult result= mockMvc.perform(get("/category/cat/"+category.getId()))
                 .andExpect(status().isOk())
                 .andReturn();
+       String reponseContent=result.getResponse().getContentAsString();
+       Category category1=objectMapper.readValue(reponseContent, Category.class);
+       Assertions.assertEquals(category.getName(),category1.getName());
+    categoryService.deleteCategory(category1.getId());
     }
 
     @Test
     void updateCategory() throws Exception {
+        Category category1=new Category();
+        category1.setName("Vehicle");
+        categoryService.saveCategory(category1);
+
         String updateCat = "{\"name\":\"Plastic\"}";
 
-        mockMvc.perform(put("/category/update/1")
+        MvcResult result= mockMvc.perform(put("/category/update/"+category1.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(updateCat))
-                .andExpect(status().isOk());
-
+                .andExpect(status().isOk())
+                .andReturn();
+        String responseContent =result.getResponse().getContentAsString();
+        Category category=objectMapper.readValue(responseContent, Category.class);
+        Assertions.assertEquals("Plastic",category.getName());
+        categoryService.deleteCategory(category.getId());
     }
 
     @Test
     void deleteCategory() throws Exception {
+        Category category=new Category();
+        category.setName("Furniture");
+        categoryService.saveCategory(category);
 
-        mockMvc.perform(delete("/category/delete/1"))
+        mockMvc.perform(delete("/category/delete/"+category.getId()))
                 .andExpect(status().isOk())
                 .andReturn();
     }
-
 }
--- src/test/java/com/catagorymanagement/integration/TaskIntegrationTest.java
+++ src/test/java/com/catagorymanagement/integration/TaskIntegrationTest.java
@@ -2,11 +2,17 @@ package com.catagorymanagement.integration;
 
 import com.catagorymanagement.Entity.Task;
 import com.catagorymanagement.Service.TaskService;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -19,19 +25,40 @@ public class TaskIntegrationTest extends BaseIntegrationTest {
     private MockMvc mockMvc;
     @Autowired
     private TaskService service;
-
-    @BeforeEach
-    public void setUp() {
-        Task task = new Task(1, "Bikes", "About Bikes", true, "High");
-        service.saveTask(task);
-    }
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     public void TaskGetIntegration() throws Exception {
+        Task task1= new Task();
+        task1.setTitle("Bikes");
+        task1.setDescription("About Bikes");
+        task1.setCompleted(true);
+        task1.setPriority("High");
+        service.saveTask(task1);
 
-        mockMvc.perform(get("/task/allTasks"))
-                .andDo(print())
-                .andExpect(status().isOk());
+        Task task2= new Task();
+        task2.setTitle("Cars");
+        task2.setDescription("About Cars");
+        task2.setCompleted(false);
+        task2.setPriority("low");
+        service.saveTask(task2);
+
+        List<Task> tasks=new ArrayList<>();
+        tasks.add(task1);
+        tasks.add(task2);
+
+        MvcResult result=mockMvc.perform(get("/task/allTasks"))
+                .andExpect(status().isOk())
+                .andReturn();
+        String responseContent= result.getResponse().getContentAsString();
+        List<Task> taskList=objectMapper.readValue(responseContent, new TypeReference<List<Task>>() {});
+        Assertions.assertEquals(task1.getTitle(),taskList.get(0).getTitle());
+        Assertions.assertEquals(task2.getTitle(),taskList.get(1).getTitle());
+
+        for (Task task:taskList){
+            service.deleteTask(task.getId());
+        }
     }
 
     @Test
@@ -55,23 +82,44 @@ public class TaskIntegrationTest extends BaseIntegrationTest {
                 "\"medium\"" +
                 "}";
 
-        mockMvc.perform(post("/task/register")
+        MvcResult result=mockMvc.perform(post("/task/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(payload))
                 .andExpect(status().isOk())
                 .andReturn();
+        String responnceContent= result.getResponse().getContentAsString();
+        Task task=objectMapper.readValue(responnceContent,Task.class);
+        Assertions.assertEquals("Men's wearing",task.getTitle());
+        service.deleteTask(task.getId());
     }
 
     @Test
     public void TaskGetBYIDIntegration() throws Exception {
+        Task task1= new Task();
+        task1.setTitle("Bikes");
+        task1.setDescription("About Bikes");
+        task1.setCompleted(true);
+        task1.setPriority("High");
+        service.saveTask(task1);
 
-        mockMvc.perform(get("/task/taskById/1"))
-                .andDo(print())
-                .andExpect(status().isOk());
+        MvcResult result= mockMvc.perform(get("/task/taskById/"+task1.getId()))
+                .andExpect(status().isOk())
+                .andReturn();
+        String reponseContent=result.getResponse().getContentAsString();
+        Task task=objectMapper.readValue(reponseContent, Task.class);
+        Assertions.assertEquals(task1.getTitle(),task.getTitle());
+        Assertions.assertEquals(task1.isCompleted(),task.isCompleted());
+        service.deleteTask(task.getId());
     }
 
     @Test
     public void TaskUpdateIntegration() throws Exception {
+        Task task1= new Task();
+        task1.setTitle("Bikes");
+        task1.setDescription("About Bikes");
+        task1.setCompleted(true);
+        task1.setPriority("High");
+        service.saveTask(task1);
 
         String updateTask = "{" +
                 "\"title\"" +
@@ -91,17 +139,27 @@ public class TaskIntegrationTest extends BaseIntegrationTest {
                 "\"medium\"" +
                 "}";
 
-
-        mockMvc.perform(put("/task/update/1")
-                        .content(updateTask)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        MvcResult result= mockMvc.perform(put("/task/update/"+task1.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateTask))
+                .andExpect(status().isOk())
+                .andReturn();
+        String responseContent =result.getResponse().getContentAsString();
+        Task task=objectMapper.readValue(responseContent, Task.class);
+        Assertions.assertEquals("Men's wearing",task.getTitle());
+        service.deleteTask(task.getId());
     }
 
     @Test
     public void TaskDeleteIntegration() throws Exception {
+        Task task1= new Task();
+        task1.setTitle("Bikes");
+        task1.setDescription("About Bikes");
+        task1.setCompleted(true);
+        task1.setPriority("High");
+        service.saveTask(task1);
 
-        mockMvc.perform(delete("/task/delete/1"))
+        mockMvc.perform(delete("/task/delete/"+task1.getId()))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
